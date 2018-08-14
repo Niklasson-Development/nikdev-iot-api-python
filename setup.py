090d1c9894e7bb5eed77052da013cc1281551d88
Johan Niklasson <johan@nicklassons.se>
2018-08-14 21:20:16 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
 setup(name='nikdev_iot',
       version='0.1',
       description='Python API for NikDev IoT server.',
-      url='https://github.com/Niklasson-Developmet/nikdev_iot_python',
+      url='https://github.com/Niklasson-Development/nikdev_iot_python',
       author='Johan Niklasson',
       author_email='johan@nik-dev.se',
       license='MIT',
