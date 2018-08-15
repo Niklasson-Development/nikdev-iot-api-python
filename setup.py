57df4dc1db02cc69714d099adf3f7674b5c484ea
Johan Niklasson <johan.niklasson@amnesty.se>
2018-08-15 17:22:52 +0200

    Added requests as dependency

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@
 from setuptools import setup
 
 setup(name='nikdev_iot',
-      version='0.1',
+      version='0.2',
       description='Python API for NikDev IoT server.',
       url='https://github.com/Niklasson-Development/nikdev_iot_python',
       author='Johan Niklasson',
       author_email='johan@nik-dev.se',
       license='MIT',
       packages=['nikdev_iot'],
+      install_requires=[
+          'requests>=2.19,<2.20',
+      ],
       zip_safe=False)
