640914826a7573c4f2b8bd005485eef9aa2854c5
Johan Niklasson <johan@nicklassons.se>
2019-03-27 16:12:13 +0100

    Added long description

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='nikdev_iot',
-      version='0.5.0rc1',
+      version='0.5.1rc1',
       description='Python API for NikDev IoT server.',
+      long_description=long_description,
       url='https://github.com/Niklasson-Development/nikdev_iot_python',
       author='Johan Niklasson',
       author_email='johan@nik-dev.se',
